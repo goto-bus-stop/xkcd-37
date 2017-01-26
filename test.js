9edf227957889fdb1c5ee1b383f59f7331d30760
René Kooi <renee@kooi.me>
2017-01-26 18:39:11 +0100

    Improve tests

--- test.js
+++ test.js
@@ -10,6 +10,8 @@ t.equal(xkcd37('Man, that\'s a sweet\-ass car'), 'Man, that\'s a sweet ass-car')
 // Combined use
 t.equal(xkcd37('Man, that\'s a sweet-ass car, but I think you\'re just a rich\\-ass fuck.'), 'Man, that\'s a sweet ass-car, but I think you\'re just a rich-ass fuck.')
 
+t.equal(xkcd37('Good-ass praxis'), 'Good ass-praxis')
+
 t.throws(function () {
   xkcd37(1337)
-})
+}, /Expected a string/)
